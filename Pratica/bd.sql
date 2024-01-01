78a8027991a3d946818ab88b89c4585127775fdb
Leandro Luiz Pereira <leandro.pereira.moon@hotmail.com>
2024-01-01 14:14:34 -0300

    Update bd.sql

--- Pratica/bd.sql
+++ Pratica/bd.sql
@@ -59,41 +59,42 @@ ADD COLUMN profissao varchar (20) FIRST ;
 -- Deletar --
 -- Deletar a coluna profissao na tabela pessoas , no banco de dados bd --
 
- ALTER TABLE pessoas 
- DROP COLUMN profissao;
+ALTER TABLE pessoas 
+DROP COLUMN profissao;
 
 -- Modificar --
 -- Modificando o tipo primitivo e constraint da coluna --
 
- ALTER TABLE pessoas
- MODIFY COLUMN profissao varchar (20) NOT NULL  ;
+ALTER TABLE pessoas
+MODIFY COLUMN profissao varchar (20) NOT NULL  ;
 
 -- Modificar o nome da coluna profissão para prof --
 
- ALTER TABLE pessoas 
- CHANGE COLUMN profissao prof varchar(40) ;
+ALTER TABLE pessoas 
+CHANGE COLUMN profissao prof varchar(40) ;
 
 -- Renomear --
 -- Renomear o nome da tabela pessoas para pessoadois --
 
- ALTER TABLE pessoas 
- RENAME TO pessoadois ;
+ALTER TABLE pessoas 
+RENAME TO pessoadois ;
 
 -- Ordenar --
 -- ORDER BY --
 -- Organizar em ordem crescente a coluna nome da tabela pessoas --
 
- SELECT * FROM pessoas
- ORDER BY nome ;
+SELECT * FROM pessoas
+ORDER BY nome ;
       -- ou --
- SELECT * FROM pessoas
- ORDER BY nome asc ;
+SELECT * FROM pessoas
+ORDER BY nome asc ;
 
 -- Organizar em ordem decrescente a coluna nome da tabela pessoas --
 
- SELECT * FROM pessoas
- ORDER BY nome desc ;
-GROUP BY
+SELECT * FROM pessoas
+ORDER BY nome desc ;
+
+-- GROUP BY --
 
 -- Além de agrupar , organizar em ordem crescente --
 
@@ -108,11 +109,13 @@ GROUP BY ano
 ORDER BY count(*) ;
 
 -- Ira selecionar toda a tabela pessoas contando a coluna ano, mostrando todos os anos que tem a contagem maior E igual a 5 , organizado por ordem crescente --
+
 SELECT ano, COUNT(*) FROM pessoas
 GROUP BY ano 
 HAVING count(ano) >= 5
 ORDER BY count(*) ;
-SELECT
+
+-- SELECT --
 
 -- Filtrar colunas --
 -- Irá selecionar apenas 3 colunas da tabelea --
@@ -122,57 +125,57 @@ ORDER BY peso ;
 
 -- Filtrar linhas --
 
- SELECT * FROM pessoas 
- WHERE ano = '2016'
- ORDER BY nome ;
+SELECT * FROM pessoas 
+WHERE ano = '2016'
+ORDER BY nome ;
 
 -- Filtrar coluna nome e ano mostrando referente ao ano 2016 --
 
- SELECT nome , ano FROM pessoas 
- WHERE ano = '2016'
- ORDER BY nome ;
+SELECT nome , ano FROM pessoas 
+WHERE ano = '2016'
+ORDER BY nome ;
 
 -- Filtrar coluna nome e ano mostrando referente ao ano menor 2015 --
 
- SELECT nome , ano FROM pessoas 
- WHERE ano <= '2015'
- ORDER BY nome ;
+SELECT nome , ano FROM pessoas 
+WHERE ano <= '2015'
+ORDER BY nome ;
 
 -- Filtrar caracteres --
 -- Filtra todos nomes que começam com a letra p --
 
- SELECT * FROM pessoas
- WHERE nome  LIKE 'p%';
+SELECT * FROM pessoas
+WHERE nome  LIKE 'p%';
 
 -- tem sempre que colocar o % depois da letra pra dar certo -- 
 -- Filtra todos nomes que terminam com a letra p --
 
- SELECT * FROM pessoas
- WHERE nome  LIKE '%p';
+SELECT * FROM pessoas
+WHERE nome  LIKE '%p';
 
 -- tem sempre que colocar o % antes da letra pra dar certo -- 
 -- Filtra todos nomes que terminam e começam com a letra p --
 
- SELECT * FROM pessoas
- WHERE nome  LIKE '%p%';
+SELECT * FROM pessoas
+WHERE nome  LIKE '%p%';
 
 -- tem sempre que colocar o % antes e depois da letra pra dar certo -- 
 -- Filtra todos nomes que terminam com a letra p e começam com ph --
 
- SELECT * FROM pessoas
- WHERE nome  LIKE 'ph%p%';
+SELECT * FROM pessoas
+WHERE nome  LIKE 'ph%p%';
 
 -- tem sempre que colocar o % antes e depois da letra pra dar certo --
 -- vai filtrar exemplos de nome photoshop , php  etc .. --
 -- Filtra todos nomes que tenham na frase leandro no inicio ou final. --
 
- SELECT * FROM pessoas
- WHERE nome  LIKE '%leandro%';
+SELECT * FROM pessoas
+WHERE nome  LIKE '%leandro%';
 
 -- Filtrar tirando duplicidade com mesmo nome . --
 -- filtrar a nacionalidade da tabela pessoas, mostrando o países cadastrados sem duplicidade --
 
-  SELECT DISTINCT Nacionalidade FROM pessoas ;
+SELECT DISTINCT Nacionalidade FROM pessoas ;
 
 -- Calculo --
 -- Total --
@@ -181,7 +184,7 @@ ORDER BY peso ;
 SELECT COUNT(*) FROM pessoas ;
 -- Mostra a quantidade total do ano cadastrado , que é maior que o ano 1990 . --
 
- SELECT COUNT(*) FROM pessoas WHERE ano > 1990 ;
+SELECT COUNT(*) FROM pessoas WHERE ano > 1990 ;
    
 -- Conta todos os registro da coluna nome da tabela cursos mostrando os registro peso , organnizado por ordem cresccente ; --
 
